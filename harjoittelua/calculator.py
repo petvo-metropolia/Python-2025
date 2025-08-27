8451b0ccdfc1545ed50b452b2bb9c02a685ffdce
petvo-metropolia <petri.vornanen@metropolia.fi>
2025-08-27 19:50:17 +0300

    noin

---
+++ harjoittelua/calculator.py
@@ -0,0 +1,8 @@
+a = float(input("Anna ensimmäinen luku: "))
+b = float(input("Anna toinen luku: "))
+
+print("Yhteenlasku:", a + b)
+print("Vähennyslasku:", a - b)
+print("Kertolasku:", a * b)
+print("Desimaalijakolasku:", a / b)
+
---
+++ tehtavat/tehtava2.py
@@ -0,0 +1,6 @@
+1.
+nimi = input("Hei, mikä on nimesi?: ")
+print("Terve,", nimi)
+
+2.
+sade = input("Mikä on ympyrän säde? ")
