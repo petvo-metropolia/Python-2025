5a72816370f4117f57e1d579943df62849d4c938
petvo-metropolia <petri.vornanen@metropolia.fi>
2025-09-03 20:04:34 +0300

    päivitys

--- tehtavat/tehtava_3.1.py
+++ tehtavat/tehtava_3.1.py
@@ -3,8 +3,9 @@
 #kuha pitää olla vähintään  37 cm
 
 pituus = int(input("Mikä on kuhan pituus (cm)?: "))
+erotus = int(37 - pituus)
 if pituus >= 37:
     print(f"Kuhan pituus on: {pituus} cm, eli kalastaminen on sallittu.")
 else:
-    print(f"Kuhan pituus on alle 37 cm, joten se on laskettava takaisin järveen.")
+    print(f"Kuha on {erotus} cm liian lyhyt, joten se on laskettava takaisin järveen.")
 
