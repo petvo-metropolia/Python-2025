c46c54045fa8af11bc9828cac191f018f41e23b2
petvo-metropolia <petri.vornanen@metropolia.fi>
2025-10-05 11:04:55 +0300

    päivitys

--- tehtavat/tehtava_5.2.py
+++ tehtavat/tehtava_5.2.py
@@ -1,1 +1,16 @@
 #tehtävä 5.2
+
+#Kysytään käyttäjältä lukuja ja tallennetaan ne listaan. Tyhjä syöte lopettaa.
+luvut = []
+luku = input("Anna luku (Enter lopettaa).: ")
+
+
+while luku !="":
+    luvut.append(luku)
+    luku = input("Anna luku (Enter lopettaa).: ")
+
+#muunnetaan luvut kokonaisluvuiksi:
+luvut = [int(luku) for luku in luvut]
+#järjestetään luvut käänteisesti ja tulostetaan:
+luvut.sort(reverse = True)
+print(luvut[0:5])
--- tehtavat/tehtava_5.3.py
+++ tehtavat/tehtava_5.3.py
@@ -1,2 +1,18 @@
 #tehtävä 5.3
 
+#Kysytään käyttäjältä luku.
+luku = int(input("Anna luku: "))
+
+#Kahta pienemmät eivät ole alkulukuja.
+if luku < 2:
+    print("Luku ei ole alkuluku.")
+else:
+    jakaja = 2
+#Kokeillaan jakaa syötetty luku kaikilla luvuilla kahdesta ylöspäin syötettyyn lukuun asti.
+    while jakaja < luku:
+        if luku % jakaja == 0:
+            print("Luku ei ole alkuluku.") #Jos luku on jaolllinen jollain luvulla ennen itseään, niin silmukka keskeytyy.
+            break
+        jakaja += 1
+    if jakaja == luku:
+        print("Luku on alkuluku.")
