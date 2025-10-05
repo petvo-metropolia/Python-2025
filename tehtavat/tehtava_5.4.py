a88e27aafcf2380ea3c13b6a966462b32417f275
petvo-metropolia <petri.vornanen@metropolia.fi>
2025-10-05 11:19:46 +0300

    päivitys

--- tehtavat/tehtava_5.4.py
+++ tehtavat/tehtava_5.4.py
@@ -1,2 +1,14 @@
 #tehtävä 5.4
 
+#Luodaan lista
+kaupungit = []
+
+#kysely jossa 5 kysymystä
+for i in range(5):
+    kaupunki = input("Anna kaupunki: ")
+    kaupungit.append(kaupunki)
+
+#tulostetaan lista
+print("Kaupungit ovat: ")
+for kaupunki in kaupungit:
+    print(kaupunki)
