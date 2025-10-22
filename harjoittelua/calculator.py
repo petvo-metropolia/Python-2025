fbd27dc19689ee4c189d1ef29ab6c611732e673d
petvo-metropolia <petri.vornanen@metropolia.fi>
2025-10-22 20:51:13 +0300

    päivitys

--- harjoittelua/calculator.py
+++ harjoittelua/calculator.py
@@ -1,18 +1,26 @@
-print("Valitse mitä toimintoa haluat käyttää:"
-      "\n A: Yhteenlasku\n B: Vähennyslasku"
-      "\n C: Kertolasku \n D: Jakolasku")
+while True:
+    print("Valitse mitä toimintoa haluat käyttää:"
+              "\n A: Yhteenlasku\n B: Vähennyslasku"
+          "\n C: Kertolasku \n D: Jakolasku \n Q: Lopeta ohjelma")
 
-valinta = input("Valintasi (A - D): ").upper()
-a = float(input("Anna ensimmäinen luku: "))
-b = float(input("Anna toinen luku: "))
+    valinta = input("Valintasi (A - D): ").upper()
 
-if valinta == "A":
-    print("Yhteenlasku:", a + b)
-elif valinta == "B":
-    print("Vähennyslasku:", a - b)
-elif valinta == "C":
-    print("Kertolasku:", a * b)
-elif valinta == "D":
-    print("Desimaalijakolasku:", a / b)
-else:
-    print("Valintasi oli virheellinen.")
+
+    if valinta == "Q":
+        print("Ohjelma lopetetaan, kiitos hei")
+        break
+
+    a = float(input("Anna ensimmäinen luku: "))
+    b = float(input("Anna toinen luku: "))
+
+    if valinta == "A":
+        print("Yhteenlasku:", a + b)
+    elif valinta == "B":
+        print("Vähennyslasku:", a - b)
+    elif valinta == "C":
+        print("Kertolasku:", a * b)
+    elif valinta == "D":
+        print("Desimaalijakolasku:", a / b)
+    else:
+        print("Valintasi oli virheellinen.")
+
---
+++ harjoittelua/rakenne.py
@@ -0,0 +1,12 @@
+#importatataan kirjastot joita tarvitaan
+import random
+import math
+
+#määritellään funktiot (ja luokat)
+def funktionnimi():
+    return
+def toinenfunktio():
+    return
+
+#pääohjelma
+print("Täällä on koodia ja funktiokutsuja")
